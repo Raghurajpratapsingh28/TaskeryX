0ddd594ed5d3301076f03344fed77315b85aae7b
Raghuraj Pratap Singh <Raghurajpratap2810@gmail.com>
2025-02-16 02:39:14 +0530

    signup update

---
+++ app/signup/page.tsx
@@ -0,0 +1,13 @@
+import { SignupFormDemo } from "@/Components/Signup";
+
+export default function Signup() {
+    return (
+    // <div className=" dark h-full w-full ">
+      
+    //   <div className=" place-content-center"> 
+       <SignupFormDemo/>
+    // </div>
+    // </div>
+   
+    );
+  }
